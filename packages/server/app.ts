b2b250d162d105015ffe0d9e368ee96ccf3cef3b
Dieter Lunn <coder2000@gmail.com>
2019-04-22 00:29:21 -0500

    refactor: change to typescript

---
+++ packages/server/app.ts
@@ -0,0 +1,20 @@
+import * as express from "express";
+import { join } from "path";
+import * as cookieParser from "cookie-parser";
+import * as logger from "morgan";
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+export default app;
---
+++ packages/server/config/webpack.config.js
@@ -0,0 +1,33 @@
+const root = require("app-root-path").path;
+
+module.exports = {
+  entry: "./bin/www.ts",
+  target: "node",
+  externals: [/^[a-z\-0-9]+$/],
+  output: {
+    filename: "compiled", // output file
+    path: `${root}/packages/server/build`,
+    libraryTarget: "commonjs"
+  },
+  resolve: {
+    // Add in `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    modules: [`${root}/node_modules`, "node_modules"]
+  },
+  resolveLoader: {
+    //root: [`${root}/node_modules`],
+  },
+  module: {
+    rules: [
+      {
+        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
